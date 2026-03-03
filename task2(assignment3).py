0d88ee6e7b83afa3c26eefeba50fcf8a2eabc999
noniponi1975-dev <noniponi1975@gmail.com>
2026-03-03 09:16:06 +0530

    Add files via upload

---
+++ task2(assignment3).py
@@ -0,0 +1,12 @@
+import math
+print("welcome!!!!!!!!!!")
+operand=float(input("enter the number on which you want to do mathmatical operation:\n "))
+square_root=math.sqrt(operand)
+print(f"square root of {operand} is {square_root}")
+
+log=math.log(operand)
+print(f"log of {operand} is {log}")
+
+
+sine_value=math.sin(operand )
+print(f"sine value of {operand} is {sine_value}")
